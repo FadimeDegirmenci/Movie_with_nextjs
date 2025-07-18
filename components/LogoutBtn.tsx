712193d95ccf0b4eb869e146ef9835c0d43bf18c
FadimeDegirmenci <fadimedegirmenci@hotmail.com>
2025-07-18 03:29:13 +0300

    full

---
+++ components/LogoutBtn.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { signOut } from 'next-auth/react';
+import React from 'react'
+
+export default function LogoutBtn() {
+  return (
+    <div 
+      className='text-center py-3 rounded-full text-white bg-neutral-900 cursor-pointer' 
+      onClick={() => {
+        signOut({
+          callbackUrl: '/signin', // Kullanıcı çıkış yaptıktan sonra yönlendirilecek sayfa
+        });
+      }}
+    >
+      Logout
+    </div>
+  )
+}
